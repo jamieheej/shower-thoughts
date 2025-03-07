6229e8edb008be616490fa8442fd4fe55caa4a81
jamieheej <jamieheejinsim@gmail.com>
2025-03-06 20:12:03 -0800

    style: add theme

---
+++ app/theme.ts
@@ -0,0 +1,21 @@
+export const lightTheme = {
+  background: "#ffffff",
+  text: "#333333",
+  primary: "#888888",
+  secondary: "#aaaaaa",
+  border: "#dddddd",
+  shadow: "rgba(0, 0, 0, 0.1)",
+  buttonBackground: "#888888",
+  buttonText: "#ffffff",
+};
+
+export const darkTheme = {
+  background: "#121212",
+  text: "#ffffff",
+  primary: "#444444",
+  secondary: "#666666",
+  border: "#333333",
+  shadow: "rgba(0, 0, 0, 0.5)",
+  buttonBackground: "#444444",
+  buttonText: "#ffffff",
+};
